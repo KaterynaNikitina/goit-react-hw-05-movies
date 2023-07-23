8dddd87261751f3b757442d1ff15a1d117d9e971
Katerina <Kateryna.e.nikitina@gmail.com>
2023-07-23 15:27:57 -0600

    pages added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from 'react';
+
+import Loader from "./Loader/Loader";
+import Layout from "./Layout/Layout";
+
+const Home = lazy(() => import ('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+
 export const App = () => {
   return (
     <div
@@ -6,11 +17,22 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         // alignItems: 'center',
-        fontSize: 40,
+        fontSize: 32,
         color: '#010101'
       }}
     >
-      React homework template
+      <Suspense fallback={<Loader />}>
+     <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />}></Route>
+        <Route path='/movies' element={<Movies /> }></Route>
+        <Route path='/movies/:movieId/*' element={<MovieDetails />}></Route>
+      </Route>
+      <Route path='*' element={<NotFound />}></Route>
+     </Routes>
+     </Suspense>
     </div>
   );
 };
+
+
